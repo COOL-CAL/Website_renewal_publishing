1552a8fe9a4b218d715ba16b439a2f74790b3e3d
GREEN <78495826+COOL-CAL@users.noreply.github.com>
2022-10-24 23:50:42 +0900

    Add files via upload

--- js/main_animation.js
+++ js/main_animation.js
@@ -1,3 +1,33 @@
+// header 
+// $(document).ready(function() {
+    // let header = document.querySelector("header");
+    // let headerHeight = header.offsetHeight;
+
+    // window.onscroll = function () {
+    //     let windowTop = window.scrollY;
+    //     if (windowTop >= 80) {
+    //         header.classList.add("on");
+    //     } else {
+    //         header.classList.remove("on");
+    //     }
+    // };
+// });
+
+function header(){
+    // 스크롤 시 header fade-in
+    $(document).on('scroll', function(){
+        if($(window).scrollTop() > 100){
+            $("#header").removeClass("deactive");
+            $("#header").addClass("active");
+        }else{
+            $("#header").removeClass("active");
+            $("#header").addClass("deactive");
+        }
+    })
+
+};
+
+
 // // Scroll Animation (sa, 스크롤 애니메이션)
 // const saDefaultMargin = 300;
 // let saTriggerMargin = 0;
@@ -32,40 +62,40 @@
 // window.addEventListener('load', saFunc);
 // window.addEventListener('scroll', saFunc);
 
-$(document).ready(function(){
-    $('.main-bg').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
-    });
-});
+// $(document).ready(function(){
+//     $('.main-bg').slick({
+//         dots: true,
+//         infinite: true,
+//         speed: 500,
+//         fade: true,
+//         cssEase: 'linear'
+//     });
+// });
 
 // Business process
 
-let moveForce = 30;
-let rotateForce = 20;
+// let moveForce = 30;
+// let rotateForce = 20;
 
-$(document).mousemove(function(e) {
-    let docX = $(document).width();
-    let docY = $(document).height();
+// $(document).mousemove(function(e) {
+//     let docX = $(document).width();
+//     let docY = $(document).height();
 
-    let moveX = (e.pageX - docX/2) / (docX/2) * -moveForce;
-    let moveY = (e.pageY - docY/2) / (docY/2) * -moveForce;
+//     let moveX = (e.pageX - docX/2) / (docX/2) * -moveForce;
+//     let moveY = (e.pageY - docY/2) / (docY/2) * -moveForce;
 
-    let rotateY = (e.pageX / docX * rotateForce)
-})
+//     let rotateY = (e.pageX / docX * rotateForce)
+// })
 
 
 // const who = document.querySelector("#mainWhoWeAre").offsetTop;
 // window.scrollTo({ top: who, behavior: "smooth" });
 
 // window.addEventListener('scroll', function() {
-function scrollToWho() {
-    const who = document.querySelector("#mainWhoWeAre").offsetTop;
-    window.scrollTo({ top: who, behavior: "smooth" });
-}
+// function scrollToWho() {
+//     const who = document.querySelector("#mainWhoWeAre").offsetTop;
+//     window.scrollTo({ top: who, behavior: "smooth" });
+// }
 // window.addEventListener('scroll', scrollToWho);
 // });
 
